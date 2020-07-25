56dca9c0d12a474e6093c1d9008c77aaed4c30b3
Ubuntu <ubuntu@ip-172-31-33-31.ec2.internal>
2020-07-25 06:54:27 +0000

    Fix gitignore and role hoist

--- src/utils.js
+++ src/utils.js
@@ -190,7 +190,7 @@ async function getSelfUrl(path = '') {
 function getMainRole(member) {
   const roles = member.roles.map(id => member.guild.roles.get(id));
   roles.sort((a, b) => a.position > b.position ? -1 : 1);
-  return roles.find(r => r.hoist);
+  return roles.find(r => r);
 }
 
 /**
